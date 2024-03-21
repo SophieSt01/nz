0a7106c5ef6374a7d67ad2443cbc1c50a1f169c1
SophieSt01 <sophie.stoffl@uibk.ac.at>
2024-03-21 17:35:34 +0100

    kommentar raus

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 
 var map = L.map('map').setView([45.9369, 7.86675], 13);
-<!--im Skripttag hier unten sagen wir dem Skript oben, was es machen muss. L steht für Leaflet, zeichne mir eine Karte, danach stehen die Koordinaten unserer Destination, als nächstes Hintergrundkarte einfügen -->
+
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
