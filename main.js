c91783474d54eff8438bb79ac5dc4340699ba03a
SophieSt01 <sophie.stoffl@uibk.ac.at>
2024-03-21 17:50:23 +0100

    Koordinaten ändern

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 
-var map = L.map('map').setView([45.9369, 7.86675], 13);
+var map = L.map('map').setView([-44.616667, 167.866667], 13);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 var marker = L.marker([45.9369, 7.86675]).addTo(map);
-marker.bindPopup("<b>Dufourspitze</b><br>4634 m").openPopup();
+marker.bindPopup("<b>Milford Sound</b>").openPopup();
 
