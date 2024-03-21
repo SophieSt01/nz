ae035b845047b1c12809fc7786bedb4a771c6097
SophieSt01 <sophie.stoffl@uibk.ac.at>
2024-03-21 17:54:03 +0100

    shortcut icon

--- main.js
+++ main.js
@@ -5,6 +5,6 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
-var marker = L.marker([45.9369, 7.86675]).addTo(map);
+var marker = L.marker([-44.616667, 167.866667]).addTo(map);
 marker.bindPopup("<b>Milford Sound</b>").openPopup();
 
