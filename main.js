f2d790c6ef2058cd12ba493349704cc09fc7deee
SophieSt01 <sophie.stoffl@uibk.ac.at>
2024-04-11 16:05:33 +0200

    zoom als variable

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
-let lat=-44.616667
-let long =167.866667
+let lat=-44.616667;
+let long =167.866667;
+let zoom = 19;
 
 var map = L.map('map').setView([lat, long], 13);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
+    maxZoom: zoom,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 var marker = L.marker([lat, long]).addTo(map);
