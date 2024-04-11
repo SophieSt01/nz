539b7e812f9046f2c638b6b27d95ea1a685b836c
SophieSt01 <sophie.stoffl@uibk.ac.at>
2024-04-11 17:27:29 +0200

    geojson

--- main.js
+++ main.js
@@ -8,10 +8,23 @@ let map = L.map('map', {
 });
 L.control.scale({imperial:false}).addTo(map);
 
+let jsonPunkt = {
+    "type": "Feature",
+    "geometry": {
+      "type": "Point",
+      "coordinates": [long, lat] //genau andersrum mit lat, long!
+    },
+    "properties": {
+      "name": "Milford Sound"
+    }
+  }
+
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+/*
 let marker = L.marker([lat, long]).addTo(map);
 marker.bindPopup(`
         <h2>Milford Sound</h2>
@@ -19,4 +32,12 @@ marker.bindPopup(`
             <li>Breite: ${lat.toFixed(5)}</li>
             <li>Länge: ${long.toFixed(5)}</li>
         `).openPopup();
-
+*/
+        // zu zeile 11ff. --> JsonPunkt
+L.geoJSON(jsonPunkt, {
+    style: function (feature) {
+        return {color: feature.properties.color};
+    }
+}).bindPopup(function (layer) {
+    return layer.feature.properties.name;
+}).addTo(map);
