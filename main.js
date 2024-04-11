8cdd3daccc5c57f5b87fd272963cccf3f11781bd
SophieSt01 <sophie.stoffl@uibk.ac.at>
2024-04-11 16:09:18 +0200

    variable zoom und `

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 let lat=-44.616667;
 let long =167.866667;
-let zoom = 19;
+let zoom = 13;
 
-var map = L.map('map').setView([lat, long], 13);
+var map = L.map('map').setView([lat, long], zoom);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: zoom,
+    maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 var marker = L.marker([lat, long]).addTo(map);
-marker.bindPopup("<b>Milford Sound</b>").openPopup();
+marker.bindPopup(`<b>Milford Sound</b>`).openPopup();
 
