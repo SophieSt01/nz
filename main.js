5c4774f0eb9a8fcebc09d846ea3084bc1cc694a8
SophieSt01 <sophie.stoffl@uibk.ac.at>
2024-04-11 16:28:52 +0200

    laeflet map neu

--- main.js
+++ main.js
@@ -2,13 +2,16 @@ let lat = -44.616667;
 let long = 167.866667;
 let zoom = 13;
 
-var map = L.map('map').setView([lat, long], zoom);
+let map = L.map('map', {
+    center: [lat, long],
+    zoom: zoom
+});
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
-var marker = L.marker([lat, long]).addTo(map);
+let marker = L.marker([lat, long]).addTo(map);
 marker.bindPopup(`
         <h2>Milford Sound</h2>
         <ul>
