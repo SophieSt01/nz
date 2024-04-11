b22a00828665b80b6596aa8c94d83d1d59fcb9ed
SophieSt01 <sophie.stoffl@uibk.ac.at>
2024-04-11 16:03:19 +0200

    lat long als variable

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
+let lat=-44.616667
+let long =167.866667
 
-var map = L.map('map').setView([-44.616667, 167.866667], 13);
+var map = L.map('map').setView([lat, long], 13);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
-var marker = L.marker([-44.616667, 167.866667]).addTo(map);
+var marker = L.marker([lat, long]).addTo(map);
 marker.bindPopup("<b>Milford Sound</b>").openPopup();
 
